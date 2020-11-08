b11d37bcec75ef955b4cff7f7603b8f79cf038e7
Ben Windsor <ben.windsor@skyrad.co.uk>
2020-11-08 12:41:32 +0000

    Add user_pool_arn output

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,8 @@ output "alternate_urls" {
   description = "Alternate URLs of the website"
   value = formatlist("https://%s", var.alternative_custom_domains)
 }
+
+output "user_pool_arn" {
+  description = "ARN of Cognito user pool being used"
+  value = var.create_cognito_pool ? module.cognito_auth.user_pool_arn : var.cognito.user_pool_arn
+}
