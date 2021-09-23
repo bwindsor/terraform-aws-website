79bb9d1a07004f8add4fd34ad2a3ab418cd5d91c
Ben Windsor <ben.windsor@skyrad.co.uk>
2021-09-23 16:56:44 +0100

    Change provider version constraints to be compatible with a wider range of provider releases

--- lambda_edge_function/main.tf
+++ lambda_edge_function/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.50.0"
+      version = ">= 3.50.0, < 4.0.0"
       configuration_aliases = [aws.us-east-1]
     }
   }
