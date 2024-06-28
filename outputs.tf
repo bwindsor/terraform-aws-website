e02b302b17c573683dde53a25663eb6199f2dfb2
Leonardo Rivera <leonardo.rivera@bemobi.com>
2024-06-28 17:21:35 -0300

    Output bucket_name

--- outputs.tf
+++ outputs.tf
@@ -17,6 +17,11 @@ output "user_pool_arn" {
   value = local.user_pool_arn
 }
 
+output "bucket_name" {
+  description = "Bucket with the website files"
+  value = aws_s3_bucket.website.bucket
+}
+
 output "data_bucket_name" {
   description = "Data bucket name, if create_data_bucket input is true"
   value = var.create_data_bucket ? aws_s3_bucket.data[0].bucket : ""
